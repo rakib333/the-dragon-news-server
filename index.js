6d7bf649716df1beed3877dce6d31747ed3457fd
rakib333 <smrakib333@gmail.com>
2023-08-04 23:26:38 +0600

    added category

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
+const categories = require('./data/categories.json');
+
 
 app.get('/', (req, res) => {
     res.send('server is running')
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
 })
 
 
